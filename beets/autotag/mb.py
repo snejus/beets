81dbffaa3d642b3e87d8c4dfcd1a2a1346763630
Šarūnas Nejus <snejus@protonmail.com>
2024-08-25 15:40:25 +0100

    Include comment field from musicbrainz

--- beets/autotag/mb.py
+++ beets/autotag/mb.py
@@ -534,6 +534,12 @@ def album_info(release: Dict) -> beets.autotag.hooks.AlbumInfo:
         data_url=album_url(release["id"]),
         barcode=release.get("barcode"),
     )
+    if (
+        annotations := musicbrainzngs.search_annotations(release["id"])[
+            "annotation-list"
+        ]
+    ) and "text" in (annotation := annotations[0]):
+        info.comment = annotation["text"]
     info.va = info.artist_id == VARIOUS_ARTISTS_ID
     if info.va:
         info.artist = config["va_name"].as_str()
