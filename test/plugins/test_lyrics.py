79ddc873498a541b6a3f5ea64b565242c563861c
Šarūnas Nejus <snejus@protonmail.com>
2024-11-04 11:53:59 +0000

    lyrics: do not search various artists, same artists sort and fix split

--- test/plugins/test_lyrics.py
+++ test/plugins/test_lyrics.py
@@ -91,7 +91,7 @@ class TestLyricsUtils:
     @pytest.mark.parametrize(
         "title, expected_extra_titles",
         [
-            ("1/2", ["1", "2"]),
+            ("1/2", []),
             ("1 / 2", ["1", "2"]),
             ("Song (live)", ["Song"]),
             ("Song (live) (new)", ["Song"]),
