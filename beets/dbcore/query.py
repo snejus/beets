swe-train/beetbox__beets

Enable querying related flexible attributes
Unify query creation logic from
- queryparse.py:construct_query_part,
- Model.field_query,
- DefaultTemplateFunctions._tmpl_unique

to a single implementation under `LibModel.field_query` class method.
This method should be used for query resolution for model (flex)fields.

Allow filtering item attributes in album queries and vice versa by
merging `flex_attrs` from Album and Item together as `all_flex_attrs`.
This field is only used for filtering and is discarded after.

beets/dbcore/query.py
@@ -144,7 +144,7 @@ class FieldQuery(Query, Generic[P]):
     @property
     def col_name(self) -> str:
         if not self.fast:
-            return f'json_extract("flex_attrs [json_str]", "$.{self.field}")'
+            return f'json_extract(all_flex_attrs, "$.{self.field}")'
 
         return f"{self.table}.{self.field}" if self.table else self.field
 

snejus/beets

beets/dbcore/query.py
@@ -128,9 +128,7 @@ class FieldQuery(Query, Generic[P]):
     @property
     def field(self) -> str:
         if not self.fast:
-            return (
-                f'json_extract("flex_attrs [json_str]", "$.{self.field_name}")'
-            )
+            return f'json_extract(all_flex_attrs, "$.{self.field_name}")'
 
         return (
             f"{self.table}.{self.field_name}" if self.table else self.field_name