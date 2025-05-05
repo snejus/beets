463997eeeb67c8d33ee670a7e3b3dcc37e1804f3
Šarūnas Nejus <snejus@protonmail.com>
2025-05-05 01:21:04 +0100

    deezer: disk_number is optional

--- beetsplug/deezer.py
+++ beetsplug/deezer.py
@@ -52,7 +52,7 @@ class Track(IDResponse):
     title: str
     artist: Artist
     track_position: int
-    disk_number: int
+    disk_number: NotRequired[int]
     duration: int
     link: str
     contributors: NotRequired[list[Artist]]
@@ -200,7 +200,7 @@ class DeezerPlugin(SearchApiMetadataSourcePlugin[Track]):
             return None
         medium_total = 0
         for i, track_data in enumerate(album_tracks_data, start=1):
-            if track_data["disk_number"] == track.medium:
+            if track_data.get("disk_number") == track.medium:
                 medium_total += 1
                 if track_data["id"] == track.track_id:
                     track.index = i
@@ -225,7 +225,7 @@ class DeezerPlugin(SearchApiMetadataSourcePlugin[Track]):
             artist_id=str(artist_id),
             length=track_data["duration"],
             index=position,
-            medium=track_data["disk_number"],
+            medium=track_data.get("disk_number"),
             deezer_track_rank=track_data.get("rank"),
             medium_index=position,
             data_source=self.data_source,
