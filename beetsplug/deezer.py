ca00ef595e742653dea7d0a64d1c9b6d0895be49
Šarūnas Nejus <snejus@protonmail.com>
2025-06-29 16:37:25 +0100

    deezer: search by query type

--- beetsplug/deezer.py
+++ beetsplug/deezer.py
@@ -253,6 +253,8 @@ class DeezerPlugin(SearchApiMetadataSourcePlugin[IDResponse]):
         :return: JSON data for the class:`Response <Response>` object or None
             if no search results are returned.
         """
+        if query_string:
+            filters[query_type] = query_string
         query = self._construct_search_query(
             query_string=query_string, filters=filters
         )
