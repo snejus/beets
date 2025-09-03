451ff9bd186f81d996074ebfd7557ef9b64ba5fc
Šarūnas Nejus <snejus@protonmail.com>
2025-09-03 19:38:34 +0100

    Handle reflink file not found error

--- beets/test/helper.py
+++ beets/test/helper.py
@@ -140,7 +140,10 @@ def check_reflink_support(path: str) -> bool:
     except ImportError:
         return False
 
-    return reflink.supported_at(path)
+    try:
+        return reflink.supported_at(path)
+    except FileNotFoundError:
+        return False
 
 
 class ConfigMixin:
