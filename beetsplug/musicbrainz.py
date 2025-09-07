30e37f0c746ba6ac482a39e60fae3d66d8f98ef2
Šarūnas Nejus <snejus@protonmail.com>
2025-09-07 14:15:18 +0100

    musicbrainz: default to media=Default Media

--- beetsplug/musicbrainz.py
+++ beetsplug/musicbrainz.py
@@ -867,6 +867,8 @@ class MusicBrainzPlugin(MetadataSourcePlugin):
         for extra_albumdata in extra_albumdatas:
             info.update(extra_albumdata)
 
+        info.media = info.media or "Digital Media"
+
         return info
 
     @cached_property
