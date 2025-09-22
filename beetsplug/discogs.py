60f1dca5a497e1874c9948bd2bdcf13cd341e019
Šarūnas Nejus <snejus@protonmail.com>
2025-09-22 12:27:26 +0100

    discogs: swap style and genre

--- beetsplug/discogs.py
+++ beetsplug/discogs.py
@@ -486,8 +486,8 @@ class DiscogsPlugin(MetadataSourcePlugin):
                 result.data.get("artists_sort")
             ),
             country=self.get_country_abbr(result.data.get("country")),
-            style=style,
-            genre=genre,
+            style=genre,
+            genre=style,
             media=media,
             original_year=original_year,
             data_source=self.data_source,
