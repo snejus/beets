d2612012607d271d1f14fe8517fde1cf5911ac0d
Šarūnas Nejus <snejus@protonmail.com>
2025-12-02 03:09:18 +0000

    dicogs: handle JSONDecodeError

--- beetsplug/discogs.py
+++ beetsplug/discogs.py
@@ -64,6 +64,7 @@ CONNECTION_ERRORS = (
     http.client.HTTPException,
     ValueError,  # JSON decoding raises a ValueError.
     DiscogsAPIError,
+    json.JSONDecodeError,
 )
 COUNTRY_OVERRIDES = {
     "Russia": "RU",  # pycountry: Russian Federation
